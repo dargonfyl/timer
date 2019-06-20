649d6eb84e38bb747d0f8ea027f765982d9bea6c
Dennis Wang <mian.wang@mail.utoronto.ca>
2019-06-20 16:41:21 -0300

    oh god why

---
+++ index.js
@@ -0,0 +1,34 @@
+var timer = 0;
+var workStatus = true; // Status is rest or work; true == work
+var interval;
+
+var workTime, restTime;
+
+var start = () => {
+	workTime = work.value;
+	restTime = rest.value;
+
+	timer = work.value;
+	console.log(timer);
+
+	countdown.innerHTML = timer;
+	status.innerHTML = "Work";
+	interval = setInterval(run, 1000);
+};
+
+var run = () => {
+	if (timer > 0) {
+		timer--;
+		console.log(timer);
+	} else {
+		if (workStatus) {
+			timer = restTime;
+			status.innerHTML = "Rest";
+		} else {
+			timer = workTime;
+			status.innerHTML = "Work";
+		}
+		workStatus = !workStatus;
+	}
+	countdown.innerHTML = timer;
+};
